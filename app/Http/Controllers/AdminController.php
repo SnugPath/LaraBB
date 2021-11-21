1c28fa13f975cc617ed22e2d74daf8822c8d0b9f
Diego Aquino <diego@aquino.site>
2021-11-21 17:28:41 -0500

    theming

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,12 +6,15 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use \Illuminate\Contracts\Foundation\Application;
 
 class AdminController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function index()
+    public function admin(): Factory|View|Application
     {
         return view('admin');
     }
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" theme="dark">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <title>LaraBB Admin Panel</title>
-    <body>
+    <body class="dark">
         <div id="app">
         </div>
         <script src="{{ mix('/js/app.js') }}"></script>
