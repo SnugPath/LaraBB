4f2e14b2b0bab697d15574d661141c8fb8bba18d
Jhosevic Castañeda <eien@wark.io>
2023-04-08 15:52:32 +0200

    Add static mapping function

--- app/Dto/ForumDto.php
+++ app/Dto/ForumDto.php
@@ -2,6 +2,7 @@
 
 namespace App\Dto;
 
+use App\Models\Forum;
 use Carbon\Carbon;
 
 class ForumDto
@@ -15,11 +16,32 @@ class ForumDto
     public int $topics_per_page;
     public int $type;
     public int $status;
-    public int $last_post;
-    public int $last_author;
+    public ?int $last_post;
+    public ?int $last_author;
     public bool $display_on_index;
     public bool $display_indexed;
     public bool $display_icons;
     public Carbon $created_at;
     public Carbon $updated_at;
+
+    public static function fromModel(Forum $forum): ForumDto
+    {
+        $dto = new ForumDto();
+        $dto->id = $forum->id;
+        $dto->parent = $forum->parent;
+        $dto->name = $forum->name;
+        $dto->desc = $forum->desc;
+        $dto->password = $forum->password;
+        $dto->img = $forum->img;
+        $dto->topics_per_page = $forum->topics_per_page;
+        $dto->type = $forum->type;
+        $dto->status = $forum->status;
+        $dto->last_post = $forum->last_post;
+        $dto->last_author = $forum->last_author;
+        $dto->display_on_index = $forum->display_on_index;
+        $dto->display_indexed = $forum->display_indexed;
+        $dto->display_icons = $forum->display_icons;
+
+        return $dto;
+    }
 }
